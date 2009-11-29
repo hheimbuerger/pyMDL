2cd5fc5b853b5577d7ebb3f9598289c6f2f28d75
Henrik Heimbuerger <henrik@heimbuerger.de>
2009-11-29 17:20:31 +0100

    Added main file template.

---
+++ pyMDL.py
@@ -0,0 +1,21 @@
+import struct
+import Image
+import sys
+
+
+
+class PyMDL:
+	def __init__(self):
+		pass
+
+	def run(self):
+		pass
+
+
+
+if(__name__ == "__main__"):
+	if len(sys.argv) < 2:
+		print "Syntax: pyMDL.py <file to convert>"
+	else:
+		pyMDL = PyMDL()
+		pyMDL.run()
